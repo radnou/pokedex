9d023ca2477558f4abdfcbf55110220670f8bb9f
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-08-24 01:48:55 +0200

    style(app): update title string to use double quotes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-pokedex-app' title`, () => {
+  it(`should have the 'pokedex' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-pokedex-app');
+    expect(app.title).toEqual('Pokedex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-pokedex-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('pokedex');
   });
 });
