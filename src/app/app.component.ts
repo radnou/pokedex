241c8729a8f3889d1321f977c983ee104ced21c2
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-08-24 22:18:55 +0200

    feat: add signal functionality for counting wins and resetting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +8,37 @@ import {Component} from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  compteur = signal(5);
+
   title: string = "Pokedex";
   name: string = "Pikachu";
-  life: number = 21;
+  wins = signal(21);
   protected readonly onclick = onclick;
 
   increment() {
-    this.life++;
+    this.wins.update(n => n + 1);
   }
 
   decrement() {
-    this.life--;
+    this.wins.update(n => n - 1);
   }
 
+  reset() {
+    this.wins.set(0);
+  }
+
+  updateSignal() {
+
+// Accès à la valeur du signal
+    console.log(this.compteur()); // 0
+
+// Modification de la valeur du signal
+    //this.compteur.set(1);
+    // console.log(this.compteur()); // 1
+
+// Mise à jour de la valeur du signal
+    this.compteur.update(n => n + 1);
+    console.log(this.compteur()); // 2
+  }
 
 }
